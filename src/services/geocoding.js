4aa96256e75f559aba0e7b2808dd79de2d2b82c6
Evgeny <jenkneo.lal@gmail.com>
2024-10-09 23:54:27 +0400

    use local storage & refactor geocoding

--- src/services/geocoding.js
+++ src/services/geocoding.js
@@ -1,6 +1,22 @@
+import { getCache, setCache } from '../utils/cache';
 const GEOCODING_API_KEY = 'f1ea1814ac5b4e5ab566d9c53b1a501a';
 
+// Нужно сделать, чтобы эта хуйня обращалась на бек
 export const getCityName = async (lat, lon) => {
+  const cacheKey = `client_city`;
+  const cachedData = getCache(cacheKey);
+
+  function cityNameByDefault() {
+    const cityName = "Астрахань"
+    console.warn(`Не удалось получить данные о городе. Установлено значение ${cityName} по умолчанию.`);
+    setCache(cacheKey, cityName);
+    return cityName
+  }
+
+  if (cachedData) {
+      return cachedData;
+  }
+
   try {
     const response = await fetch(
       `https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lon}&key=${GEOCODING_API_KEY}&language=ru&pretty=1`
@@ -8,12 +24,13 @@ export const getCityName = async (lat, lon) => {
     const data = await response.json();
     if (data.results.length > 0) {
       const components = data.results[0].components;
-      return components.city || components.town || components.village || 'Неизвестный город';
+      const cityName = components.city || components.town || components.village || 'Неизвестный город';
+      setCache(cacheKey, cityName);
+      return cityName
     } else {
-      return 'Неизвестный город';
+      return cityNameByDefault()
     }
   } catch (error) {
-    console.error('Ошибка при обратном геокодировании:', error);
-    return 'Неизвестный город';
+      return cityNameByDefault()
   }
 };
