9a85c8658692d7ffb66d41d4c74fae933f79b891
Evgeny <jenkneo.lal@gmail.com>
2024-10-10 00:27:46 +0400

    dev mode & prod mode

--- src/services/airPollution.js
+++ src/services/airPollution.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const isDevMode = process.env.NODE_ENV === 'development'
+
 const API_KEY = '7eef441a87868e8991b28fec50f95b7e'; // Замените на ваш API-ключ
-const BASE_URL = 'http://api.openweathermap.org/data/2.5/air_pollution';
+const BASE_URL = (isDevMode ? 'http' : 'https') + `://api.openweathermap.org/data/2.5/air_pollution`;
 
 export const getAirPollutionData = async (lat, lon) => {
   try {
