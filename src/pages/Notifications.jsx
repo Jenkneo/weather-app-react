c06bb96e4134f845c563f97eb4464db629dab1a9
Evgeny <jenkneo.lal@gmail.com>
2024-10-10 00:08:36 +0400

    ПОФИКСИЛ ЕБУЧУЮ ОШИБКУ, ДЯДЯ РИНАТ ГОВНЕЦО НЕ ПОДТЕР ЗА СОБОЙ ИЗ-ЗА ЧЕГО В ПРОД НАХУЙ НЕ ПУШИЛОСЬ

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { getHealthRecommendations, airQualityOptions } from '../hooks/airQualityRecommendations'; 
+import { airQualityOptions } from '../hooks/airQualityRecommendations'; 
 
 const NotificationsContainer = styled.div`
   padding: 20px;
