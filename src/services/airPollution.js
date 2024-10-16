e7e740db791aabb869821b2e63b6ed4841a5fcb7
Dmitry <103138302+nuafirytiasewo@users.noreply.github.com>
2024-10-17 00:01:16 +0400

    minor comments

--- src/services/airPollution.js
+++ src/services/airPollution.js
@@ -5,9 +5,12 @@ const isDevMode = process.env.NODE_ENV === 'development'
 const API_KEY = '7eef441a87868e8991b28fec50f95b7e'; // Замените на ваш API-ключ
 const BASE_URL = (isDevMode ? 'http' : 'https') + `://api.openweathermap.org/data/2.5/air_pollution`;
 
+// Функция для получения данных о загрязнении воздуха
 export const getAirPollutionData = async (lat, lon) => {
   try {
+    //отправляем гет на сервер
     const response = await axios.get(`${BASE_URL}?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
+    //получаем данные
     return response.data;
   } catch (error) {
     console.error('Ошибка при получении данных о загрязнении воздуха:', error);
@@ -15,9 +18,12 @@ export const getAirPollutionData = async (lat, lon) => {
   }
 };
 
+// Функция для получения прогноза загрязнения воздуха
 export const getAirPollutionForecast = async (lat, lon) => {
   try {
+    //отправляем гет на сервер
     const response = await axios.get(`${BASE_URL}/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
+    //получаем данные
     return response.data;
   } catch (error) {
     console.error('Ошибка при получении прогноза загрязнения воздуха:', error);
